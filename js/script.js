3bb86a99851251cbe6b0dbbe2adfd9ca74938da0
Veia Kurman <veiakurman@gmail.com>
2022-09-22 16:56:04 -0500

    Added Num Generator for Computer Choice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/* scissors > paper; paper > rock; rock > scissors
+ *
+ * - Get random number between 0-2 to represent R/P/S choice for computer
+ * - Get user input to decide their choice
+ * - Do logic to figure out which choice wins
+ * - Update count of winner, do next round
+ * - Repeat this until winner count is 5
+ */
+
 function getComputerChoice() {
-  
+  function getRandomInt() {
+    return Math.floor(Math.random() * 3);
+  }
 }
