549362753fe49bf39694b0300784b0ac773246fc
Veia Kurman <veiakurman@gmail.com>
2022-09-22 17:12:48 -0500

    Create Computer Rock/Paper/Scissors Choice Generator

--- js/script.js
+++ js/script.js
@@ -8,7 +8,24 @@
  */
 
 function getComputerChoice() {
+  // makes number between 0-2
   function getRandomInt() {
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 3); 
   }
+
+  let compChoice; // initialize for use in if..else loop
+  let compNum = getRandomInt();
+
+  // assigns rock/paper/scissors choice based on random number generator
+  if (compNum === 0) {
+    compChoice = 'rock';
+  } else if (compNum === 1) {
+    compChoice = 'paper';
+  } else if (compNum === 2) {
+    compChoice = 'scissors';
+  } else {
+    console.log('Computer Number Generator Broken');
+  }
+
+  return compChoice;
 }
