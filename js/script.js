5143133e8ebac249d36e3f133c9660eddc516764
Veia Kurman <veiakurman@gmail.com>
2022-09-22 21:50:20 -0500

    Format Files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* 
+/*
  *
  * - Get random number between 0-2 to represent R/P/S choice for computer
  * - Get user input to decide their choice
@@ -7,64 +7,82 @@
  * - Repeat this until winner count is 5
  */
 
+// Initialize counters to keep score
+let compScore = 0;
+let playerScore = 0;
+
+// generates computer choices using random numbers
 function getComputerChoice() {
-  // makes number between 0-2
-  function getRandomInt() {
-    return Math.floor(Math.random() * 3); 
-  }
+    // ranomly generates 0, 1, or 2 int
+    function getRandomInt() {
+        return Math.floor(Math.random() * 3);
+    }
 
-  let compChoice; // initialize for use in if..else loop
-  let compNum = getRandomInt();
+    let compChoice; // initialize for use later in loop
+    let compNum = getRandomInt();
 
-  // assigns rock/paper/scissors choice based on random number generator
-  if (compNum === 0) {
-    compChoice = 'rock';
-  } else if (compNum === 1) {
-    compChoice = 'paper';
-  } else if (compNum === 2) {
-    compChoice = 'scissors';
-  } else {
-    console.log('Computer Number Generator Broken');
-  }
+    // assigns rock/paper/scissors choice based on random number generator
+    if (compNum === 0) {
+        compChoice = "rock";
+    } else if (compNum === 1) {
+        compChoice = "paper";
+    } else if (compNum === 2) {
+        compChoice = "scissors";
+    } else {
+        console.log("Computer Number Generator Broken");
+    }
 
-  return compChoice;
+    return compChoice;
 }
 
 function playRound(playerInput) {
-  // initialize for later
-  let result;
-  // define player and computer choice for processing
-  let playerChoice = playerInput.toLowerCase();
-  let compChoice = getComputerChoice();
+    // initialize for later
+    let result;
+    // define player and computer choice for processing
+    let playerChoice = playerInput.toLowerCase();
+    let compChoice = getComputerChoice();
 
-  if (playerChoice === 'rock' && compChoice === 'paper') {
-    result = 'Computer wins, paper beats rock!';
-    return result;
-  } else if (playerChoice === 'rock' && compChoice === 'scissors') {
-    result = 'You win, rock beats scissors!';
-    return result;
-  } else if (playerChoice === 'rock' && compChoice === 'rock') {
-    result = 'Tie, you both chose rock!';
-    return result;
-  } else if (playerChoice === 'scissors' && compChoice === 'paper') {
-    result = 'You win, rock beats paper!';
-    return result;
-  } else if (playerChoice === 'scissors' && compChoice === 'scissors') {
-    result = 'Tie, you both chose scissors!';
-    return result;
-  } else if (playerChoice === 'scissors' && compChoice === 'rock') {
-    result = 'Computer wins, rock beats scissors!';
-    return result;
-  } else if (playerChoice === 'paper' && compChoice === 'paper') {
-    result = 'Tie, you both chose paper!';
-    return result;
-  } else if (playerChoice === 'paper' && compChoice === 'scissors') {
-    result = 'Computer wins, paper beats rock!';
-    return result;
-  } else if (playerChoice === 'paper' && compChoice === 'rock') {
-    result = 'You win, paper beats rock!';
-    return result;
-  } else {
-    console.log('Damn son, where\'d you find this?');
-  }
+    /* rock > scissors
+     * paper > rock
+     * scissors > paper
+     * scissors === scissors
+     * paper === paper
+     * rock === rock
+     */
+    if (playerChoice === "rock" && compChoice === "paper") {
+        result = "Computer wins, paper beats rock!";
+        return result;
+    } else if (playerChoice === "rock" && compChoice === "scissors") {
+        result = "You win, rock beats scissors!";
+        return result;
+    } else if (playerChoice === "rock" && compChoice === "rock") {
+        result = "Tie, you both chose rock!";
+        return result;
+    } else if (playerChoice === "scissors" && compChoice === "paper") {
+        result = "You win, rock beats paper!";
+        return result;
+    } else if (playerChoice === "scissors" && compChoice === "scissors") {
+        result = "Tie, you both chose scissors!";
+        return result;
+    } else if (playerChoice === "scissors" && compChoice === "rock") {
+        result = "Computer wins, rock beats scissors!";
+        return result;
+    } else if (playerChoice === "paper" && compChoice === "paper") {
+        result = "Tie, you both chose paper!";
+        return result;
+    } else if (playerChoice === "paper" && compChoice === "scissors") {
+        result = "Computer wins, paper beats rock!";
+        return result;
+    } else if (playerChoice === "paper" && compChoice === "rock") {
+        result = "You win, paper beats rock!";
+        return result;
+    } else {
+        console.log("Damn son, where'd you find this?");
+    }
+}
+
+function game() {
+    let userInput = prompt("What's your move?");
+    let result = playRound(userInput);
+    console.log(result);
 }
